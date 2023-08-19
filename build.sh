a9d7c3738833aa8bb13826ae2eb375c25ba7580a
Sergio Valverde <toto@vlvrd.me>
2023-08-18 21:04:12 -0600

    Add social media links

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ echo "Building..."
 [ ! -d "public" ] && mkdir $BUILD_DIR
 cp index.html $BUILD_DIR/index.html
 cp style.css $BUILD_DIR/style.css
+cp -r images $BUILD_DIR/images
 
 echo "Bondo Built!"
 echo "ボンド"
